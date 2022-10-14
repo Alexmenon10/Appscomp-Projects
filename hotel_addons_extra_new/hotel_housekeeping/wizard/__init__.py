84ae033710eb36ae770a458e29e6af35c1b68ecd
praveenmr09 <praveen.appscomp@gmail.com>
2022-10-14 15:48:02 +0530

    [REMOVE] Removed Unwanted Module

--- hotel_addons_extra_new/hotel_housekeeping/wizard/__init__.py
+++
@@ -1,4 +0,0 @@
-# See LICENSE file for full copyright and licensing details.
-
-from . import housekeeping_wizard
-from . import  cancel_wizard
--- hotel_addons_extra_new/hotel_restaurant/wizard/__init__.py
+++
@@ -1,4 +0,0 @@
-# See LICENSE file for full copyright and licensing details.
-
-from . import hotel_restaurant_wizard
-from . import cancel_wizard
